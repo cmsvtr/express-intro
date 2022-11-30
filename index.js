1b7b849cda9f81b7480e44abe75d37990c54297c
Vitor Martins Costa <costa.vsm@gmail.com>
2022-11-30 07:51:27 -0300

    com modelo

--- index.js
+++ index.js
@@ -1,46 +1,19 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import uuid from "uuid";
+import connect from "./config/db.config.js"
+import userRoute from "./routes/processos";
 
-dotenv.config()
-
-app.use(express.json())
+dotenv.config();
 
 const app = express();
 
+app.use(express.json());
 
-app.get("/all", (req, res) => {
-    return res.status(200).json(bancodedados)
-})
+connect();
 
-app.post("/create", (req, res) => {
+app.use("/user", userRoute);
 
-    bancodedados.push(req.body)
-
-    return res.status(201).json(bancodedados)
-})
-
-app.put("/edit/:id", (req, res) => {
-
-    const editById = bancodedados.find(obj => obj.id === req.params.id)
-    const index = bancodedados.indexOf(editById)
-
-    bancodedados.splice(index, 1, req.body)
-    
-    return res.status().json(bancodedados)
-    return res.status().json(bancodedados)
-})
-
-app.delete("/delete/:id", (req, res) => {
-    
-    const deleteById = bancodedados.find(obj => obj.id === req.params.id)
-    const index = bancodedados.indexOf(deleteById)
-
-    bancodedados.splice(index, 1)
-    
-    return res.status().json(bancodedados)
-})
 
 app.listen(process.env.PORT, () => {
-    console.log("Server is up and running on port " + process.env.PORT)
+    console.log(`App up and running on port http://localhost:${process.env.PORT}`);
 })
